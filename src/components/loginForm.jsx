2fbdf17dabc8b1585ed87802d6f9e95338142d58
Azamat Bekmuratov <azamat.bekmuratov24@gmail.com>
2019-09-12 09:04:20 -0700

    first commit

---
+++ src/components/loginForm.jsx
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+import Input from "./common/input";
+
+class LoginForm extends Component {
+  state = {
+    account: { username: "", password: "" },
+    errors: {}
+  };
+
+  validate = () => {
+    return { username: "Username is required" };
+  };
+  // componentDidMount() {
+  //   this.username.current.focus();
+  // }
+  handleSubmit = e => {
+    e.preventDefault();
+
+    const errors = this.validate();
+
+    this.setState({ errors });
+
+    if (errors) return;
+    //Call the server
+    console.log("submitted");
+  };
+
+  handleChange = ({ currentTarget: input }) => {
+    const account = { ...this.state.account };
+    account[input.name] = input.value;
+    this.setState({ account });
+  };
+
+  render() {
+    const { account } = this.state;
+    return (
+      <div>
+        <h1>Login</h1>
+        <form onSubmit={this.handleSubmit}>
+          <Input
+            name="username"
+            label="Username"
+            value={account.username}
+            onChange={this.handleChange}
+          />
+          <Input
+            name="password"
+            label="Password"
+            value={account.password}
+            onChange={this.handleChange}
+          />
+          <button className="btn btn-primary">Login</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default LoginForm;
